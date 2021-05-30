b8b314cb9832d2db31ad7bdc5ab317fe0043909f
davide <davide.schiaroli2@studio.unibo.it>
2021-05-30 10:14:00 +0200

    Create client with create-react-app

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # rankMe
 
 progetto applicazioni e servizi web
+
+Per lanciare il server:
+
+#npm run server
+
+Per il client:
+
+#npm run client
+
+Per entrambi:
+
+#npm run dev
--- client/pages/home.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Home extends Component {
-    render() {
-        return <p>Welcome to Home!</p>
-    }
-}
-
-export default Home;
