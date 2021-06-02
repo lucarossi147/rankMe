70382ecfc59c2a82787b468fa9c34958a836c43e
davide <davide.schiaroli2@studio.unibo.it>
2021-06-02 11:06:38 +0200

    Modified server, created alternative login/signup for testing hope it works

--- server/index.js
+++ server/index.js
@@ -11,22 +11,25 @@ const LOCAL_DB = 'mongodb://localhost:27017/rankMe';
 const DOCKER_DB = 'mongodb://mongodbContainer.rankMe_interna:27017';
 
 // aspetto 10 sec che il container di mongo sia su
-function pausecomp(millis)
+/*function pausecomp(millis)
 {
     var date = new Date();
     var curDate = null;
     do { curDate = new Date(); } while(curDate-date < millis);
 }
-pausecomp(10000);
+pausecomp(10000);*/
 
 //connessione al db
 mongoose.set('useFindAndModify', false);
-mongoose
-    .connect(
-        LOCAL_DB,
-        { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch((err) => console.log(err));
+mongoose.connect(
+        LOCAL_DB, {
+            useFindAndModify: false,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => console.log('MongoDB Connected'))
+          .catch((err) => console.log(err));
+
+//TODO: risolvere problema di CORS app.use(cors), così pare che funzioni
 app.use(cors())
 
 app.use(express.json());
